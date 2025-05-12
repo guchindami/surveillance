977b08b337295d4089cff775a7f399e93634fffd
Uchindami Gondawe <143083979+guchindami@users.noreply.github.com>
2025-05-12 18:40:07 +0200

    Explicitly bind to 0.0.0.0

--- server.js
+++ server.js
@@ -892,4 +892,4 @@ process.on('SIGTERM', () => {
 });
   // Kill all active FFmpeg
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port,  '0.0.0.0', () => console.log(`Server listening on http://0.0.0.0:${port}`));
